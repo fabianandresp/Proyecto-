b7c2f3edd3caaa1b7da69cbb4f32149a60f2643b
pimie <pimie@192.168.0.25>
2020-10-30 21:08:45 -0600

    Creacion de java class de problemas

---
+++ src/problema3/Problema3.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problema3;
+
+/**
+ *
+ * @author pimie
+ */
+public class Problema3 {
+    //Impuesto sobre la renta
+    
+    int salario;
+    int [] dependiente = {1,2,3,4};
+    double [] gravable = {100, 90, 70, 60, 40};
+    
+    
+}
---
+++ src/problema4/añoBisiesto.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problema4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pimie
+ */
+public class añoBisiesto {
+    
+    public static void main (String[] args){
+        
+    Scanner input = new Scanner(System.in);
+    
+    int año;
+    año = input.nextInt();
+    
+    
+    }  
+}
