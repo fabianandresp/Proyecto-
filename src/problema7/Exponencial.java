5b1e5946dd551196cda70569487e393bf97c0d6f
pimie <pimie@192.168.0.25>
2020-11-04 21:30:31 -0600

    Termino del problema 7

--- src/problema7/Exponencial.java
+++ src/problema7/Exponencial.java
@@ -5,10 +5,31 @@
  */
 package problema7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pimie
  */
 public class Exponencial {
     
+    public static void main(String[] arg){
+        Scanner input = new Scanner(System.in);
+        
+        
+        int numero;
+        int exponente;
+        int resultado=1 ;
+        
+        System.out.println("Ingrese el numero x: ");
+        numero = input.nextInt();
+        System.out.println("Cuantas veces desea que se multiplique ese numero?");
+        exponente = input.nextInt();
+        
+        
+        for (int i=1; i<=exponente; i++){
+            resultado = resultado * numero;
+        }
+        System.out.println("El resultado final es: " + resultado);
+    }
 }
