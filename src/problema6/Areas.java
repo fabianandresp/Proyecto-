5f35f2f5d73235bb95e346746132997f360ea114
pimie <pimie@192.168.0.25>
2020-11-05 23:19:30 -0600

    Termino del problema 6

--- src/problema6/Areas.java
+++ src/problema6/Areas.java
@@ -1,8 +1,78 @@
+
+import java.util.Scanner;
+
+
+public class Areas {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int opcion;
+        double h;
+        double b;
+        int r;
+        double res;
+        double res1;
+        double p = 3.1416;
+        do {
+
+            System.out.println("BIENVENIDO AL SISTEMA!");
+            System.out.println("1.- ÁREA DEL TRIANGULO");
+            System.out.println("2.- ÁREA DEL CUADRADO");
+            System.out.println("3.- ÁREA DEL CIRCULO");
+            System.out.println("4.- SALIR DEL SISTEMA");
+            System.out.println("ELIGE UNA OPCION: ");
+            
+            opcion = input.nextInt();
+            
+  
+
+            switch (opcion) {
+
+                case 1:
+                    System.out.println("DAME UN VALOR PARA LA BASE DEL TRIANGULO: ");
+                    b = input.nextInt();
+                    System.out.println("DAME EL VALOR PARA LA ALTURA DEL TRIANGULO:  ");
+                    h = input.nextInt();
+                    res = (b * h) / 2;
+                    System.out.println("EL ÁREA DEL TRIANGULO ES: " + res);
+                    break;
+
+                case 2:
+                    System.out.println("DAME EL VALOR PARA LA BASE DEL CUADRADO: ");
+                    b = input.nextInt();
+                    res = b * b;
+                    System.out.println("EL AREA DEL CUADRADO ES: " + res);
+                    break;
+
+                case 3:
+                    System.out.println("DAME EL VALOR PARA EL RADIO: ");
+                    r = input.nextInt();
+                    res1 = (p * r * r);
+                    System.out.println("EL ÁREA DEL CIRCULO ES: " + res1);
+                    break;
+
+                case 4:
+                    System.out.println("GRACIAS POR VISITARNOS VUELVE CUANDO GUSTES!!!");
+                    break;
+
+                default:
+                    System.out.println("LA OPCIÓN ELEGIDA NO SE ENCUENTRA EN EL SISTEMA");
+                    break;
+
+            }
+
+        } while (op != 4);
+
+    }
+
+}
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
+ 
 package problema6;
 
 import java.util.Scanner;
@@ -10,8 +80,8 @@ import java.util.Scanner;
 /**
  *
  * @author pimie
- */
-public class Areas {
+ 
+
     public static void main (String[] args){
     Scanner input = new Scanner(System.in);
         
@@ -48,4 +118,4 @@ public class Areas {
             
     }
     }
-}
+}*/
