3e972aa7201aec5c1f5fef361ee70a268b55540b
pimie <pimie@192.168.0.25>
2020-12-04 20:59:25 -0600

    Envio del Quiz 2

--- src/poo/problema9/Tester.java
+++ src/poo/problema9/Tester.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class Tester {
-    
+
     public static void main(String args[]) {
-        
+
         SistemaNotas sistema = new SistemaNotas();
         Scanner s = new Scanner(System.in);
         Menu me = new Menu();
         Alumno alum = new Alumno();
-        
+
         me.opciones();
         System.out.println("¿Que opcion desea?");
         while (true) {
-            
+
             int opc = s.nextInt();
-            
+
             switch (opc) {
                 case 1:
                     System.out.println("Digite el nombre del alumno:");
@@ -26,13 +26,13 @@ public class Tester {
                     String y = s.next();
                     System.out.println("Digite el carne:");
                     int z = s.nextInt();
-                    
+
                     alum.setNombre(x);
                     alum.setApellido(y);
                     alum.setCarne(z);
-                    
+
                     me.opciones2();
-                    
+
                     break;
                 case 2:
                     System.out.println("Digite la nota obtenida:");
@@ -41,7 +41,7 @@ public class Tester {
                     int puntosTotales = s.nextInt();
                     System.out.println("Digite puntos obtenidos:");
                     int puntosObtenidos = s.nextInt();
-                    
+
                     Examen e = new Examen(alum, nota, puntosTotales,
                             puntosObtenidos, null);
                     sistema.agregarEx(e);
@@ -52,50 +52,41 @@ public class Tester {
                     String n = s.next();
                     System.out.println("Digite el apellido del Padre");
                     String a = s.next();
-                    
+
                     PadreFamilia prueba = new PadreFamilia(n, a);
                     alum.setPadre(prueba);
-                    
+
                     System.out.println("Digite el nombre de la Madre");
                     String n2 = s.next();
                     System.out.println("Digite el apellido de la Madre");
                     String a2 = s.next();
-                    
+
                     MadreFamilia prueba2 = new MadreFamilia(n2, a2);
                     alum.setMadre(prueba2);
-                    
+
                     me.opcionNew();
-                    
+
                     break;
                 case 4:
                     alum.mostrardatos();
                     sistema.mostrarExamenes();
                     alum.imprimirPadreFamlia();
-                    me.opciones4();
-                    
+
                     break;
                 case 5:
-                    
+
                     System.out.println("ADIOS PROFE!");
                     System.out.println(" ");
                     
-                    me.opciones();
-                    System.out.println("¿Que opcion desea?");
-                
+                    break;
+
+                   
+
             }
+             
 
-
-            /*
-            Alumno a = new Alumno();
-            System.out.println("Digite el nombre del alumno:");
-            a.setNombre(s.next());
-            System.out.println("Digite el nombre del alumno:");
-            a.setApellido(s.next());
-            System.out.println("Digite el carne:");
-            a.setCarne(s.nextInt());
-             */
         }
-        
+
     }
-    
+
 }
